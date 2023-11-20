2296cf560b18328ee57840680c3c93e52cc64bf9
Adisie Fenta <adisiefenta23@gmail.com>
2023-11-20 14:34:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className='container'>
+                <h3>react</h3>
+            </div>
+        )
+    }
+}
+
+
+export default App;
